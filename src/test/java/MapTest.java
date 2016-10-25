5992d454abff2c13083e765051b71634a0f2c1f6
skuli <skulia15@ru.is>
2016-10-25 16:34:27 +0000

    Map class partially complete with test

--- src/test/java/MapTest.java
+++ src/test/java/MapTest.java
@@ -2,16 +2,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MapTest {
-    Map map = new Map();
     @Test public void testHasWon() {
+	Map map = new Map();
         assertEquals(0, map.hasWon());
     }
 
     @Test public void testSetMove() {
-        assertEquals(true, map.setMove(0,0));
+	Map map = new Map();
+        assertEquals(true, map.setMove(1,3));
     }
  
     @Test public void testGetPoint() {
+	Map map = new Map();
         assertEquals(0, map.getPoint(0));
     }
 }
