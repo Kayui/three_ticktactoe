57a4646e07fb05a6a8e91423d984172b566328e2
skuli <skulia15@ru.is>
2016-10-30 22:04:57 +0000

    added test for tictactoe and console

--- src/test/java/ConsoleTest.java
+++ src/test/java/ConsoleTest.java
@@ -2,10 +2,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ConsoleTest {
-    @Test public void testMsgBox() {
+    @Test public void testDisplayMap() {
         Console console = new Console();
+	Map map = new Map();
+	console.displayMap(map);
 	//String testString = "this is a reliable test"; 
 	//assertEquals("this is a reliable test", console.msgbox(testString));
 	//find out how to test
     }    
+
+    @Test public void testPickOption() {
+        Console console = new Console();
+	String message = "message";
+	console.msgbox(message);
+    }
 }
